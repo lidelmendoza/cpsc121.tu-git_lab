749c4136a533435c34a2e7bf5114aba143a67b7c
lidelmendoza <lidelmendoza9@csu.fullerton.edu>
2017-03-25 23:59:49 -0700

    This is the header: LMendoza.h

---
+++ LMendoza.h
@@ -0,0 +1,21 @@
+
+// convert miles into kilometers
+// there are 1.6 kilometers in a mile
+#include <iostream>
+using namespace std;
+#include "LMendoza.h"
+
+void miles_to_kilomtrs(){
+  int choice;
+  double kilomtr = 1.60934;
+  cout<< "Enter a whole number of miles and I will convert it into kilometers\n";
+  cin >> choice;
+  while (!isdigit(choice)){// not a letter
+    cout << "Invalid input \n Please try again\n";
+    cin.clear();
+    cin.ignore(100000000,'\n');
+    cin >> choice;
+  }
+  kilomtr*=choice;
+  cout << "You entered " << choice << " mile(s)\n" << "In kilometers it is " << kilomtr << "\n";
+}
