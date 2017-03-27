76f2c47c4b5ae67aa33c44b55a8dd382d5800174
Tyler <tzlopez13@gmail.com>
2017-03-26 19:20:09 -0700

    added main function (measures of fun)

---
+++ headers/tlopez.h
@@ -0,0 +1,11 @@
+int tlopez() {
+	int megaFonzies;
+	double kiloBravos;
+	cout << "How many MegaFonzies are you currently having? ";
+	cin << megaFonzies;
+	kiloBravos = megaFonzies*325;
+	cout << "\nThat means that you are currently having "
+	<< kiloBravos << " KiloBravos!\n";
+	cin.get();
+	return 0;
+}
